81242ad842579f08573960c896a6da1d13e8eadf
ZachKellogg <zachskellogg@gmail.com>
2020-06-17 22:20:01 -0600

    fixed linting errors

--- models/location.js
+++ models/location.js
@@ -1,37 +1,36 @@
 module.exports = (sequelize, DataTypes) => {
-    const Location = sequelize.define("Location",{
-       city: {
-           type: DataTypes.STRING,
-           allowNull: false
-       },
-        picture: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        decription: {
-            type: DataTypes.TEXT,
-            allowNull: false
-        },
-        cost:{
-            type: DataTypes.INTEGER,
-            allowNull: false
-        },
-        address: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        rating: {
-            type: DataTypes.INTEGER,
-            allowNull: false
-        }
-        
-    } )
-    Location.associate = function(models) {
-        Location.belongsTo(models.Planet, {
-            foreignKey: {
-                allowNull: false
-              }
-        });
-      };
-    return Location;
-}
+  const Location = sequelize.define("Location", {
+    city: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    picture: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    decription: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    cost: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    rating: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+  });
+  Location.associate = function(models) {
+    Location.belongsTo(models.Planet, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
+  return Location;
+};
