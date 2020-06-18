03024dc15e7649abeb6328242c7d03f9fed20f64
ZachKellogg <zachskellogg@gmail.com>
2020-06-17 19:08:14 -0600

    some of my changes

--- Seed.sql
+++ Seed.sql
@@ -1,5 +1,10 @@
 INSERT INTO planets (id, name, star, Sector, moon, rotation, rebel, empire, blackSuns)
-VALUES (1, "Kashyyyk", 1, "Mytaranor", 3, 26, true, false, false);
+VALUES (1, "Kashyyyk", "Mytaranor", 3, 26, true, false, false);
+VALUES (2, "Mustafar", "Atravis", 0, 36, false, true, false);
+VALUES (3, "Naboo", "Chommell", 3, 26, true, false, false);
+VALUES (4, "Ryloth", "Gaulus", 3, , true, false, false);
+VALUES (5, "Kamino", "Extragalactic", 3, 27, false, true, true);
+VALUES (6, "Concord Dawn", "Mytaranor", 3, 26, true, false, false);
 
 INSERT INTO locations (city, picture, decription, cost, address, rating, PlanetId)
-Values (Kachiro, "https://i.pinimg.com/originals/c9/0c/8c/c90c8cbe4f4b875a755815ecf6e43f23.jpg", "Kachirho was a coastal Wookiee settlement located in the Wawaatt Archipelago of Kashyyyk that served as the planet's capital. It was made up of several inhabited wroshyr trees, including Tree Kachirho and Tree Vikkilynn. Two piers and docks extend out from the city into the freshwater lagoon. During the Clone Wars it was led by the chieftain Tarfful. Near the end of the Clone Wars, Kachirho was the site of the Battle of Kashyyyk", 36,000, Life tree, 1)
+Values (Kachiro, "https://i.pinimg.com/originals/c9/0c/8c/c90c8cbe4f4b875a755815ecf6e43f23.jpg", "Kachirho was a coastal Wookiee settlement located in the Wawaatt Archipelago of Kashyyyk that served as the planet's capital. It was made up of several inhabited wroshyr trees, including Tree Kachirho and Tree Vikkilynn. Two piers and docks extend out from the city into the freshwater lagoon. During the Clone Wars it was led by the chieftain Tarfful. Near the end of the Clone Wars, Kachirho was the site of the Battle of Kashyyyk", 36,000, );
