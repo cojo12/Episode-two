4ba8eab2fa4f0448557cb95975307fc019862c74
ZachKellogg <zachskellogg@gmail.com>
2020-06-17 22:17:26 -0600

    created models and planets, also seeded the DB

--- Seed.sql
+++ Seed.sql
@@ -1,10 +1,16 @@
-INSERT INTO planets (id, name, star, Sector, moon, rotation, rebel, empire, blackSuns)
-VALUES (1, "Kashyyyk", "Mytaranor", 3, 26, true, false, false);
-VALUES (2, "Mustafar", "Atravis", 0, 36, false, true, false);
-VALUES (3, "Naboo", "Chommell", 3, 26, true, false, false);
-VALUES (4, "Ryloth", "Gaulus", 3, , true, false, false);
-VALUES (5, "Kamino", "Extragalactic", 3, 27, false, true, true);
-VALUES (6, "Concord Dawn", "Mytaranor", 3, 26, true, false, false);
+Use Episode2DB;
 
-INSERT INTO locations (city, picture, decription, cost, address, rating, PlanetId)
-Values (Kachiro, "https://i.pinimg.com/originals/c9/0c/8c/c90c8cbe4f4b875a755815ecf6e43f23.jpg", "Kachirho was a coastal Wookiee settlement located in the Wawaatt Archipelago of Kashyyyk that served as the planet's capital. It was made up of several inhabited wroshyr trees, including Tree Kachirho and Tree Vikkilynn. Two piers and docks extend out from the city into the freshwater lagoon. During the Clone Wars it was led by the chieftain Tarfful. Near the end of the Clone Wars, Kachirho was the site of the Battle of Kashyyyk", 36,000, );
+INSERT INTO planets 
+	(id, name, star, sector, moon, rotation, rebel, empire, blackSuns)
+VALUES 
+(1, "Kashyyyk", "Mytaranor", 3, 26, true, false, false),
+(2, "Mustafar", "Atravis", 0, 36, false, true, false),
+(3, "Naboo", "Chommell", 3, 26, true, false, false),
+(4, "Ryloth", "Gaulus", 3, true, false, false),
+(5, "Kamino", "Extragalactic", 3, 27, false, true, true),
+(6, "Concord Dawn", "Mytaranor", 3, 26, true, false, false);
+
+INSERT INTO locations 
+	(city, picture, decription, cost, address, rating, PlanetId)
+Values 
+	(Kachiro, " ", "Located in a friendly, quiet and safe neighborhood called GGRrtgggrrr, this home is 2 blocks west of the capitol and a short walk or bike trip to downtown and the hover train. Fully stocked with fridge and stove, this cozy and bright unit with a great view, is decorated in 2630s retro style, and has a comfy queen luxury firm bed and pull out couch. On-site credit laundry. Pet friendly but please contact me before booking to review policy.", 36,000, "118 W. Apricot", 4.93, 1);
